a763f8159aa631e85bffc497c2371c6ff8d36cb5
Allie <alliesafronova@gmail.com>
2024-08-08 20:12:28 +0700

    feat: add test fixture

--- sorting/tests/tests_entry.cpp
+++ sorting/tests/tests_entry.cpp
@@ -1,52 +1,46 @@
 #include "tests_entry.hpp"
 
-TEST(selection, test1) {
-  std::vector v1{100, 35, 500, 9, 67, 20};
-  std::vector v2{25, 0, 500, 56, 98};
-  std::vector v1_exp{9, 20, 35, 67, 100, 500};
-  std::vector v2_exp{0, 25, 56, 98, 500};
+class Sorting : public testing::Test {
+ protected:
+  void SetUp() override {
+    v1_ = {100, 35, 500, 9, 67, 20};
+    v2_ = {25, 0, 500, 56, 98};
+    v1_exp_ = {9, 20, 35, 67, 100, 500};
+    v2_exp_ = {0, 25, 56, 98, 500};
+  }
 
-  //   printVector(v1);
-  selectionSort(v1.begin(), v1.end());
-  EXPECT_EQ(v1, v1_exp);
-  //   printVector(v1);
+  std::vector<int> v1_;
+  std::vector<int> v2_;
+  std::vector<int> v1_exp_;
+  std::vector<int> v2_exp_;
+};
 
-  //   printVector(v2);
-  selectionSort(v2.begin(), v2.end());
-  EXPECT_EQ(v2, v2_exp);
-  //   printVector(v2);
+TEST_F(Sorting, selection_1) {
+  EXPECT_NE(v1_, v1_exp_);
+  selectionSort(v1_.begin(), v1_.end());
+  EXPECT_EQ(v1_, v1_exp_);
+
+  EXPECT_NE(v2_, v2_exp_);
+  selectionSort(v2_.begin(), v2_.end());
+  EXPECT_EQ(v2_, v2_exp_);
 }
 
-TEST(bubble, test1) {
-  std::vector v1{100, 35, 500, 9, 67, 20};
-  std::vector v2{25, 0, 500, 56, 98};
-  std::vector v1_exp{9, 20, 35, 67, 100, 500};
-  std::vector v2_exp{0, 25, 56, 98, 500};
+TEST_F(Sorting, bubble_1) {
+  EXPECT_NE(v1_, v1_exp_);
+  bubbleSort(v1_.begin(), v1_.end());
+  EXPECT_EQ(v1_, v1_exp_);
 
-  //   printVector(v1);
-  bubbleSort(v1.begin(), v1.end());
-  EXPECT_EQ(v1, v1_exp);
-  //   printVector(v1);
-
-  //   printVector(v2);
-  bubbleSort(v2.begin(), v2.end());
-  EXPECT_EQ(v2, v2_exp);
-  //   printVector(v2);
+  EXPECT_NE(v2_, v2_exp_);
+  bubbleSort(v2_.begin(), v2_.end());
+  EXPECT_EQ(v2_, v2_exp_);
 }
 
-TEST(insertion, test1) {
-  std::vector v1{100, 35, 500, 9, 67, 20};
-  std::vector v2{25, 0, 500, 56, 98};
-  std::vector v1_exp{9, 20, 35, 67, 100, 500};
-  std::vector v2_exp{0, 25, 56, 98, 500};
+TEST_F(Sorting, insertion_1) {
+  EXPECT_NE(v1_, v1_exp_);
+  insertionSort(v1_.begin(), v1_.end());
+  EXPECT_EQ(v1_, v1_exp_);
 
-  //   printVector(v1);
-  insertionSort(v1.begin(), v1.end());
-  EXPECT_EQ(v1, v1_exp);
-  //   printVector(v1);
-
-  //   printVector(v2);
-  insertionSort(v2.begin(), v2.end());
-  EXPECT_EQ(v2, v2_exp);
-  //   printVector(v2);
+  EXPECT_NE(v2_, v2_exp_);
+  insertionSort(v2_.begin(), v2_.end());
+  EXPECT_EQ(v2_, v2_exp_);
 }
